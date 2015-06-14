81acdcc58f7045b837e495d58aec8328c9f22e32
Marco Pivetta <ocramius@gmail.com>
2015-06-14 22:35:15 +0100

    post-construct checker experiment

--- src/StrictPhp/StrictPhpKernel.php
+++ src/StrictPhp/StrictPhpKernel.php
@@ -2,14 +2,22 @@
 
 namespace StrictPhp;
 
+use Go\Aop\Features;
 use Go\Core\AspectContainer;
 use Go\Core\AspectKernel;
-use StrictPhp\Aspect\ImmutableProperty;
 use StrictPhp\Aspect\ImmutablePropertyCheck;
+use StrictPhp\Aspect\PostConstructStateCheck;
 use StrictPhp\Aspect\PropertyWriteTypeCheck;
 
 class StrictPhpKernel extends AspectKernel
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @override
+     */
+    protected $options = ['features' => Features::INTERCEPT_INITIALIZATIONS];
+
     /**
      * {@inheritDoc}
      */
@@ -17,5 +25,6 @@ class StrictPhpKernel extends AspectKernel
     {
         $container->registerAspect(new PropertyWriteTypeCheck());
         $container->registerAspect(new ImmutablePropertyCheck());
+        $container->registerAspect(new PostConstructStateCheck());
     }
 }
